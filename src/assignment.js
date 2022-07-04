9898c31d1d8b30c2d6e244532bcb492ca0ea1dbc
cuddle-bunny <bluebun07@gmail.com>
2022-07-04 13:21:22 +0100

    commented out module.export

--- src/assignment.js
+++ src/assignment.js
@@ -67,6 +67,6 @@ function celsiusToFahrenheit(arrayOfNumbers) {
 
 // ========================
 // DO NOT EDIT THIS BLOCK
-//module.exports = assignment;
+module.exports = assignment;
 // ========================
 
